e8bd27ff6ed667e165c9bebb8effaf125c61ef0f
Gage Hundertmark <hundertmarkgage@gmail.com>
2022-09-14 22:45:18 -0600

    added partial functionality for querying and displaying select github projects with a 24 hour server cache period.

---
+++ src/routes/projects/+page.server.js
@@ -0,0 +1,27 @@
+let lastTime;
+let cachedRepos;
+
+/** @type {import('./$types').PageServerLoad} */
+export const load = async () => {
+
+    if (cachedRepos) if ((Date.now()-lastTime) < 24*60*60*1000) return {repos: cachedRepos};
+
+    console.log("Querying for any github updates...")
+
+    const allRepos = (await (await fetch("https://api.github.com/search/repositories?q=user:Gagezilla+topic:showcase")).json()).items
+    const repos = allRepos.map(repo => {
+        if (repo.topics.includes("showcase")) {
+            let name = repo.name
+            let desc = repo.description
+            let img = "https://raw.githubusercontent.com/Gagezilla/" + repo.name + "/" + repo.default_branch + "/showcase.png"
+            return {name, desc, img}
+        }
+    }).filter(repo => repo)
+
+    lastTime = Date.now()
+    cachedRepos = repos
+
+    return {
+        repos
+    }
+}
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,17 @@
 <script>
     import Page from "../comps/Page.svelte";
+    export let data;
 </script>
 
 <Page title="Projects" content="This is the projects page.">
-    <p class="text-center font-medium text-3xl mt-20">
-        <span class="text-6xl block mb-8">🔨😁👍</span>
-        Projects page in progress...
-    </p>
+    {#each data.repos as repo}
+        <h1>{repo.name}</h1>
+        <p>{repo.desc}</p>
+        <img src={repo.img} alt="">
+    {/each}
 </Page>
+
+<!--<p class="text-center font-medium text-3xl mt-20">-->
+<!--    <span class="text-6xl block mb-8">🔨😁👍</span>-->
+<!--    Projects page in progress...-->
+<!--</p>-->
