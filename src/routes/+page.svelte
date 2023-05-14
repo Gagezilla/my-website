d869846dd27df5e81a17451bcc4fe4358c0fe430
Gage Hundertmark <52170630+Gagezilla@users.noreply.github.com>
2023-05-14 17:30:07 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,9 @@
     import Page from "./comps/Page.svelte";
 </script>
 
-<Page notoppad title="Home" content="This is the home page.">
-    <div class="relative h-[20rem] md:h-[25rem] overflow-hidden rounded-b-3xl">
-        <img alt="avatar" src="/img/avatar.png" class="scale-110 w-full h-full object-cover blur-xl">
-        <div class="top-0 w-full h-full bg-black/50 absolute"></div>
-        <h1 class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-medium text-3xl md:text-5xl whitespace-nowrap">
-            👋 Hi, I'm Gage.
-        </h1>
-    </div>
+<Page notoppad title="Home"content="This is the home page.">
+    <p class="text-center font-medium text-3xl mt-20">
+        <span class="text-6xl block mb-8">🔨😁👍</span>
+        Website in progress...
+    </p>
 </Page>
